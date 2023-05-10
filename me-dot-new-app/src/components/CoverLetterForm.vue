2e4137ea59cff5400626dd2180c05d91816b68e8
PhilippBuschhaus <buschhaus00@gmail.com>
2023-05-10 20:35:17 +0200

    new project

---
+++ me-dot-new-app/src/components/CoverLetterForm.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container>
+    <v-form @submit.prevent="submit">
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            v-model="fullName"
+            :rules="textRules"
+            required
+            counter="30"
+            name="Your Name"
+            label="Your Name"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            v-model="companyName"
+            :rules="textRules"
+            required
+            counter="30"
+            name="Company Name"
+            label="Name of company you're applying to"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            v-model="position"
+            :rules="textRules"
+            required
+            counter="30"
+            name="Position"
+            label="Position you're applying for"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12" sm="6">
+          <v-text-field
+            v-model="experience"
+            :rules="textRules"
+            required
+            counter="30"
+            name="Most relevant experience"
+            label="Most relevant experience / project"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row class="pb-3">
+        <v-btn block type="submit" color="success">Generate</v-btn>
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { watch } from "vue";
+
+const watcher = watch({});
+//form data
+const fullName = ref("");
+const companyName = ref("");
+const position = ref("");
+const experience = ref("");
+
+//input validation
+const isValid = ref(false);
+const textRules = ref([
+  (value) => {
+    if (!value) {
+      return "Input Required";
+    }
+    isValid.value = true;
+    return true;
+  },
+]);
+
+// !!submit!!
+function submit() {
+  //only submit if the data is valid
+  if (isValid.value) {
+    //creating the object to send to our worker
+    const formDataToSubmit = {
+      fullName: fullName.value,
+      companyName: companyName.value,
+      position: position.value,
+      experience: experience.value,
+    };
+    //SEND IT
+    console.log(formDataToSubmit);
+  }
+}
+</script>
---
+++ me-dot-new-app/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container>
+    <!-- Headline -->
+    <v-card>
+      <div class="text-h4 text-md-h2 pb-5 pt-5 text-center">
+        Ai Cover Letter Generator
+      </div>
+      <div class="text-subtitle-2 text-md-subtitle-1 text-center pb-5">
+        Generate a personalised cover letter in a few minutes!
+      </div>
+
+      <!-- Form -->
+      <CoverLetterForm></CoverLetterForm>
+    </v-card>
+  </v-container>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import CoverLetterForm from "@/components/CoverLetterForm.vue";
+</script>
